78076c7e74bc90fac2bd3f7a94657056474d973e
botsConfig <81992668+botsConfig@users.noreply.github.com>
2021-04-06 12:29:53 +0200

    Update script.sh

--- Fedora/script.sh
+++ Fedora/script.sh
@@ -2,6 +2,6 @@
 
 sudo dnf install i3-gaps feh compton firefox alacritty kitty flatpak codeblocks dmenu fish
 rm ~/.config/i3/config
-cp i3c/config ~/.config/i3/config
-cp vimrc/.vimrc ~/.vimrc
-cp wall/wall.jpg ~/wall.jpg
+cp config ~/.config/i3/config
+cp .vimrc ~/.vimrc
+cp wall.jpg ~/wall.jpg
