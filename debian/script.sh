44bb3055320cbabb2d28ae45582ddd3c4e37187a
botsConfig <81992668+botsConfig@users.noreply.github.com>
2021-04-06 10:46:35 +0200

    Update script.sh

--- debian/script.sh
+++ debian/script.sh
@@ -3,5 +3,5 @@
 sudo apt install i3-gaps feh compton firefox alacritty kitty flatpak codeblocks dmenu fish
 rm ~/.config/i3/config
 cp i3c/config ~/.config/i3/config
-cp vimrc/.vimrc ~/.vimrc
+cp .vimrc ~/.vimrc
 cp wall/wall.jpg ~/wall.jpg
