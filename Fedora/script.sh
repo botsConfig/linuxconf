2b62ed1e9325f2d42a5e1bbb8d17da45f072f275
botsConfig <81992668+botsConfig@users.noreply.github.com>
2021-04-06 12:45:21 +0200

    Update script.sh

--- Fedora/script.sh
+++ Fedora/script.sh
@@ -3,5 +3,5 @@
 sudo dnf install vim i3 feh compton firefox alacritty kitty flatpak codeblocks dmenu fish
 rm ~/.config/i3/config
 cp config ~/.config/i3/config
-cp .vimrc ~/.vimrc
+cp .vimrc ~/
 cp wall.jpg ~/wall.jpg
